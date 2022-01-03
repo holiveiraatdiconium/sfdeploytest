1eb9cd189da8c934349ff04be8c3a173bc5000ec
Helder Oliveira <helder.oliveira@diconium.com>
2022-01-03 12:34:28 +0000

    Adding api

---
+++ SfHelloWorldApi/HelloWorldService/Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly ILogger<HelloController> _logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<DummyObject> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new DummyObject
+            {
+                Name = Faker.Name.FullName()
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ SfHelloWorldApi/HelloWorldService/WeatherForecast.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace HelloWorldService
+{
+    public class DummyObject
+    {
+        public string Name { get; set; }
+    }
+}
